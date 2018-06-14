51f20be72e747b9bb9defb33e0c150467bf2dd89
Enrico Höschler <enrico.hoeschler@gmail.com>
2018-06-14 12:45:19 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+# basic packages needed
+sudo apt-get install git php npm
+
+# travisci cli tools
+gem install travis -v 1.8.8 --no-rdoc --no-ri
+
+# heroku cli tools
+sudo snap install heroku --classic
